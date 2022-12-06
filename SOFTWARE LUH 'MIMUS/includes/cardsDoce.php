ddf533a6c451d50de04a8667d7a851d1b97d7282
Melissa Neves <116177595+nevesmelissa@users.noreply.github.com>
2022-12-06 20:51:58 -0300

    Update cardsDoce.php

--- SOFTWARE LUH 'MIMUS/includes/cardsDoce.php
+++ SOFTWARE LUH 'MIMUS/includes/cardsDoce.php
@@ -1,10 +1,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="../css/bootstrap.css">
-    <link rel="stylesheet" href="../css/cards.css">
-    <link rel="stylesheet" href="../vendors/bootstrap-icons/bootstrap-icons.css">
-    <link rel="shortcut icon" href="../images/favicon.svg" type="image/x-icon">
+  
 </head>
 
 <div class="cards">
