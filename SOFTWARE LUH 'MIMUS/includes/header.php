76ab884848f78d9f8678cf6ba80be77aa347f252
devmim26 <yasmin.boaventura2004@gmail.com>
2022-12-07 03:06:39 -0300

    .

--- SOFTWARE LUH 'MIMUS/includes/header.php
+++ SOFTWARE LUH 'MIMUS/includes/header.php
@@ -7,17 +7,12 @@
                                 <span Class="me-50"><img src="../images/user.png" alt="" width="32" height="32" class="rounded-circle me-2"></span><a" style="margin-top:20px;" &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp> <?php echo $_SESSION['nome'];?></a>
                             </button>
                             <div class="dropdown-menu" aria-labelledby="dropdownMenuButtonEmoji">
-                                <a class="dropdown-item" href="#"><span class="dropdown-item-emoji"></span>
-                                    <div class="form-check form-switch custom-switch">
-                                        <input class="form-check-input" type="checkbox" id="flexSwitchCheckDefault">
-                                        <label class="form-check-label" for="flexSwitchCheckDefault">Tema escuro</label>
-                                    </div>
-                                </a>
-                                <a class="dropdown-item" href="./manual.html"><span class="dropdown-item-emoji"><svg style="width: 21px;" xmlns="http://www.w3.org/2000/svg" width="12" height="12" fill="currentColor" class="bi bi-gear-fill" viewBox="0 0 16 16">
+                                
+                                <a class="dropdown-item" href="../view/manual.php"><span class="dropdown-item-emoji"><svg style="width: 21px;" xmlns="http://www.w3.org/2000/svg" width="12" height="12" fill="currentColor" class="bi bi-gear-fill" viewBox="0 0 16 16">
                                             <path d="M9.405 1.05c-.413-1.4-2.397-1.4-2.81 0l-.1.34a1.464 1.464 0 0 1-2.105.872l-.31-.17c-1.283-.698-2.686.705-1.987 1.987l.169.311c.446.82.023 1.841-.872 2.105l-.34.1c-1.4.413-1.4 2.397 0 2.81l.34.1a1.464 1.464 0 0 1 .872 2.105l-.17.31c-.698 1.283.705 2.686 1.987 1.987l.311-.169a1.464 1.464 0 0 1 2.105.872l.1.34c.413 1.4 2.397 1.4 2.81 0l.1-.34a1.464 1.464 0 0 1 2.105-.872l.31.17c1.283.698 2.686-.705 1.987-1.987l-.169-.311a1.464 1.464 0 0 1 .872-2.105l.34-.1c1.4-.413 1.4-2.397 0-2.81l-.34-.1a1.464 1.464 0 0 1-.872-2.105l.17-.31c.698-1.283-.705-2.686-1.987-1.987l-.311.169a1.464 1.464 0 0 1-2.105-.872l-.1-.34zM8 10.93a2.929 2.929 0 1 1 0-5.86 2.929 2.929 0 0 1 0 5.858z" />
                                         </svg></span>
                                     ‎ ‎ ‎ ‎ Ajuda</a>
-                                    <a class="dropdown-item" href="./manual.html"><span class="dropdown-item-emoji"><svg xmlns="http://www.w3.org/2000/svg" width="12" height="12" fill="currentColor" class="bi bi-person" viewBox="0 0 16 16">
+                                    <a class="dropdown-item" href=""><span class="dropdown-item-emoji"><svg xmlns="http://www.w3.org/2000/svg" width="12" height="12" fill="currentColor" class="bi bi-person" viewBox="0 0 16 16">
   <path d="M8 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6Zm2-3a2 2 0 1 1-4 0 2 2 0 0 1 4 0Zm4 8c0 1-1 1-1 1H3s-1 0-1-1 1-4 6-4 6 3 6 4Zm-1-.004c-.001-.246-.154-.986-.832-1.664C11.516 10.68 10.289 10 8 10c-2.29 0-3.516.68-4.168 1.332-.678.678-.83 1.418-.832 1.664h10Z"/>
 </svg></span>
                                     ‎ ‎ ‎ ‎ Perfil</a>
