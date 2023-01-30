b0eab26b76c6c298076d192ab52d95f7e665a2da
Neutron_369 <79660502+T-hawan@users.noreply.github.com>
2023-01-29 21:54:17 -0300

    atualização do menu header

--- includes/menuHeaderBar.php
+++ includes/menuHeaderBar.php
@@ -3,7 +3,7 @@
             <div class="btn-group mb-1">
                 <div class="btn-group mb-1" id="meu">
                     <div class="dropdown dropdown-color-icon user">
-                        <button class="btn  dropdown-toggle" type="button" id="dropdownMenuButtonEmoji" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" style="width:40">
+                        <button class="btn  dropdown-toggle" type="button" id="dropdownMenuButtonEmoji" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                             <span Class="me-50"><img src="../resources/images/user.png" alt="" width="32" height="32" class="rounded-circle me-2"></span>nome do usuário
                         </button>
                         <div class="dropdown-menu" aria-labelledby="dropdownMenuButtonEmoji">
@@ -22,7 +22,7 @@
                                     </svg></span>
                                 ‎ ‎ ‎ ‎ Perfil</a>
                             <hr>
-                            <a class="dropdown-item" href="../index.html"><span class="dropdown-item-emoji"><svg style="width: 21px;" xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-box-arrow-right" viewBox="0 0 16 16">
+                            <a class="dropdown-item" href="../index.php"><span class="dropdown-item-emoji"><svg style="width: 21px;" xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-box-arrow-right" viewBox="0 0 16 16">
                                         <path fill-rule="evenodd" d="M10 12.5a.5.5 0 0 1-.5.5h-8a.5.5 0 0 1-.5-.5v-9a.5.5 0 0 1 .5-.5h8a.5.5 0 0 1 .5.5v2a.5.5 0 0 0 1 0v-2A1.5 1.5 0 0 0 9.5 2h-8A1.5 1.5 0 0 0 0 3.5v9A1.5 1.5 0 0 0 1.5 14h8a1.5 1.5 0 0 0 1.5-1.5v-2a.5.5 0 0 0-1 0v2z" />
                                         <path fill-rule="evenodd" d="M15.854 8.354a.5.5 0 0 0 0-.708l-3-3a.5.5 0 0 0-.708.708L14.293 7.5H5.5a.5.5 0 0 0 0 1h8.793l-2.147 2.146a.5.5 0 0 0 .708.708l3-3z" />
                                     </svg></span>
